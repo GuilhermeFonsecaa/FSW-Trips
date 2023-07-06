fa239aa4b727cd8ded1f6ca8a5a66fa665070520
Guilherme Fonseca <guilhermao1234@hotmail.com>
2023-07-05 22:35:51 -0300

    feat: add next auth provider

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,4 +1,4 @@
-import { prisma } from '../../lib/prisma'
+import { prisma } from '../../../lib/prisma'
 import * as React from 'react';
 
 const getTrips = async () => {
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function Home() {
+  return (
+    <div>Hello World</div>
+  )
+}
